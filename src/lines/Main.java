3d490db8a85f8d85565f063eb9f1a70bba6e6ae4
egorcouldlie <couldliegor@gmail.com>
2022-12-14 15:09:15 +0300

    TASK 1

--- src/lines/Main.java
+++ src/lines/Main.java
@@ -1,12 +1,14 @@
 package lines;
-
 public class Main {
     public static void main(String[] args) {
         task1();
     }
-
     public static void task1() {
-
-
+        System.out.println("\nTASK1\n");
+        String firstName = "Ivan ";
+        String middleName = "Ivanov ";
+        String lastName = "Ivanovich ";
+        System.out.println("Full name is: " + firstName + middleName + lastName);
     }
+    //done
 }
