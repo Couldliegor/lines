e622349ebcef7f54d7869f09b61e49436051d4ce
egorcouldlie <couldliegor@gmail.com>
2022-12-14 15:13:26 +0300

    TASK 2

--- src/lines/Main.java
+++ src/lines/Main.java
@@ -1,8 +1,11 @@
 package lines;
+
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
+
     public static void task1() {
         System.out.println("\nTASK1\n");
         String firstName = "Ivan ";
@@ -10,5 +13,15 @@ public class Main {
         String lastName = "Ivanovich ";
         System.out.println("Full name is: " + firstName + middleName + lastName);
     }
+
     //done
+    public static void task2() {
+        System.out.println("\nTASK2\n");
+        String firstName = "Ivan ";
+        String middleName = "Ivanov ";
+        String lastName = "Ivanovich ";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("Full name for report : " + fullName.toUpperCase());
+    }
+
 }
